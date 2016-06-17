be598bce8513c87b453077be6f406ce02e0c0221
irrelon <rob@irrelon.com>
2016-06-17 13:14:45 +0100

    Removed all ForerunnerDB references from require.config.js

--- src/app/require.config.js
+++ src/app/require.config.js
@@ -9,20 +9,9 @@ var require = {
         "knockout": "bower_modules/knockout/dist/knockout",
         "knockout-projections": "bower_modules/knockout-projections/dist/knockout-projections",
         "signals": "bower_modules/js-signals/dist/signals.min",
-        "text": "bower_modules/requirejs-text/text",
-        // ForeunnerDB
-        "forerunner": "bower_modules/forerunnerdb/js/dist/fdb-all.min",
-        // Forerunner Db dependencies
-        "async": "bower_modules/async/dist/async",
-        "crypto-js":"bower_modules/crypto-js//crypto-js",
-        "pako": "bower_modules/pako/dist/pako.min",
-        "rest":"bower_modules/rest/client/default",
-
-      
+        "text": "bower_modules/requirejs-text/text"
     },
     shim: {
-        "bootstrap": { deps: ["jquery"] },
-        // ForeunnerDB shim
-        "forerunner": { deps: ["async", "pako","crypto-js"] }
+        "bootstrap": { deps: ["jquery"] }
     }
 };
